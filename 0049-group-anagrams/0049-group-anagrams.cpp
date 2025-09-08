5d6c99c8ab9a909c769f0e3844cdf2365f5311aa
Harvey <44375598+kurz3m3@users.noreply.github.com>
2025-09-08 14:32:17 -0400

    0049-GROUP-ANAGRAMS Stats: Time: 11 ms (91.58%), Space: 25.1 MB (62.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> results;
-        unordered_map<string, vector<string>> wordMap;
-        for(string s : strs){
-            string sortedS = s;
-            sort(sortedS.begin(), sortedS.end());
-            wordMap[sortedS].push_back(s);
+        // Need to check each string and group similar words
+        // return vector of vectors with with anagrams in the same nested vectors
+        // Handle empty case and words with no matches
+        if(strs.empty()){
+            return {{""}};
         }
+        
+        // Naive solution:
+         // sort each word and then find matches
+         // O(n log n) time complexity?
+         // use hashmap to find matches
 
-        for(auto& it : wordMap){
-            results.push_back(it.second);
+        
+        unordered_map<string,vector<string>> wordMap;
+        for(const auto& c : strs){
+            string sortedC = c;
+            sort(sortedC.begin(), sortedC.end());
+            wordMap[sortedC].push_back(c);
         }
+        vector<vector<string>> result;
+        for(auto& m : wordMap){
+            result.push_back(m.second);
+        }
+        
 
-        return results;
+
+        // Better solution:
+         // bucket sort? Have array of alphabet and count letters?
+        //unordered_map<string, >
+        
+        return result;
     }
 };
